aec2baccbcaeb6613b9b35b53b6c94fb38e9aba4
Alumnos Santiago Centro <alumnos09@inacap.cl>
2026-03-20 20:34:35 -0300

    holi

---
+++ tarea.py
@@ -0,0 +1,15 @@
+#desarrollo y programa que permita ingresar 5 datos de un usuario
+#ocupa los 3 tipos de datos decimales y alfa numericos
+# en un print con toda la informacion
+# 5 input
+name ="" # declaracion de variable
+edad =""
+altura =""
+pelicula ="" 
+sexo =""
+name = input ( " cual es su nombre") #usuario debe responder
+edad = input ("cual es tu edad ")
+altura = input ( " cual es tu altura")
+pelicula = input (" cual es tu pelicula favorita")
+sexo = input ("que sexo eres")
+print (" hola",name, "tienes",edad, "años", "su altura es",altura," su pelicula favorita",pelicula, "su sexo es",sexo, )
